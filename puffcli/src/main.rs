09430edf2c25366c42755d320812377aa802dc90
BlueHotDog <BlueHotDog@users.noreply.github.com>
2019-05-30 17:44:52 +0300

    more work

--- puffcli/src/main.rs
+++ puffcli/src/main.rs
@@ -1,21 +1,24 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::{Arg, App, SubCommand};
 
 fn main() {
     let matches = App::new("Puff")
+        .about("Like NPM but for Protobuffers")
         .version(crate_version!())
         .author(crate_authors!())
-        .about("wget clone written in Rust")
-        .arg(
-            Arg::with_name("URL")
-                .required(true)
-                .takes_value(true)
-                .index(1)
-                .help("url to download"),
-        )
+        .subcommand(loginSubCommand())
         .get_matches();
     let url = matches.value_of("URL").unwrap();
     println!("{}", url);
+}
+
+
+fn loginSubCommand() -> clap::App {
+    return SubCommand::with_name("test")
+        .about("Adds a registy user account")
+        .arg(Arg::with_name("debug")
+            .short("d")
+            .help("print debug information verbosely"))
 }
